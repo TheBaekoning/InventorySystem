1974716df7b4a2130d93b945a74e44dc55b2d729
thebaekoning <baekdoug@gmail.com>
2020-06-06 09:45:37 -0700

    -added functionality for delete and searching

--- src/main/controller/MainMenuController.java
+++ src/main/controller/MainMenuController.java
@@ -1,34 +1,89 @@
 package main.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import main.model.*;
 
-
-import javafx.event.ActionEvent;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.Node;
-
 public class MainMenuController implements Initializable {
     @FXML
     private TextField partSearchField;
+    @FXML
+    private TextField productSearchField;
+
+    public TableView<Part> partTable;
+    public TableView<Product> productTable;
+    public TableColumn partIdColumn;
+    public TableColumn partNameColumn;
+    public TableColumn partInvColumn;
+    public TableColumn partCostColumn;
+    public TableColumn productIdColumn;
+    public TableColumn productNameColumn;
+    public TableColumn productInvColumn;
+    public TableColumn productCostColumn;
+
+    public Inventory inventory = new Inventory();
+
+    public int partId = 1;
+    public int productId = 1;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        inventory.addPart(new InHouse(partId++, "2", 3, 4, 5, 6, 7));
+        inventory.addPart(new InHouse(partId++, "23", 3.2, 5, 5, 6, 8));
+        inventory.addPart(new InHouse(partId++, "24", 31, 6, 5, 6, 9));
+        partIdColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        partNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        partInvColumn.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        partCostColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+        partTable.setItems(inventory.getAllParts());
+
+        inventory.addProduct(new Product(productId++, "2", 3, 4, 5, 6));
+        productIdColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        productNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        productInvColumn.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        productCostColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+        productTable.setItems(inventory.getAllProducts());
     }
 
     @FXML
     private void searchParts() {
-        System.out.println(partSearchField.getCharacters());
-        String searchString = (String) partSearchField.getCharacters();
+        CharSequence searchString = partSearchField.getCharacters();
+        int searchInt = Integer.parseInt(searchString.toString());
+
+        for (int i = 0; i < inventory.getAllParts().size(); i++) {
+            if (inventory.getAllParts().get(i).getId() == searchInt) {
+                partTable.getSelectionModel().select(i);
+                break;
+            }
+        }
+    }
+
+    @FXML
+    private void searchProducts() {
+        CharSequence searchString = productSearchField.getCharacters();
+        int searchInt = Integer.parseInt(searchString.toString());
+
+        for (int i = 0; i < inventory.getAllProducts().size(); i++) {
+            if (inventory.getAllProducts().get(i).getId() == searchInt) {
+                productTable.getSelectionModel().select(i);
+                break;
+            }
+        }
     }
 
     @FXML
@@ -70,5 +125,23 @@ public class MainMenuController implements Initializable {
     @FXML
     private void clickExitButton() {
         System.exit(0);
+    }
+
+    @FXML
+    private void clickDeletePart() {
+        inventory.deletePart(partTable.getSelectionModel().getSelectedItem());
+    }
+
+    @FXML
+    private void clickDeleteProduct() {
+        inventory.deleteProduct(productTable.getSelectionModel().getSelectedItem());
+    }
+
+    public void addPart(InHouse inHouse) {
+        inventory.addPart(inHouse);
+    }
+
+    public void addPart(OutSourced outSourced) {
+        inventory.addPart(outSourced);
     }
 }
--- src/main/controller/PartController.java
+++ src/main/controller/PartController.java
@@ -6,11 +6,49 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import main.model.InHouse;
+import main.model.OutSourced;
 
 import java.io.IOException;
 
 public class PartController {
+    @FXML
+    RadioButton inHouseButton;
+
+    @FXML
+    RadioButton outSourcedButton;
+
+    @FXML
+    TextField partId;
+
+    @FXML
+    TextField partName;
+
+    @FXML
+    TextField partInv;
+
+    @FXML
+    TextField partCost;
+
+    @FXML
+    TextField partMax;
+
+    @FXML
+    TextField partMin;
+
+    @FXML
+    TextField partCompany;
+
+    @FXML
+    Label miscName;
+
+    @FXML
+    Button savePartButton;
 
     @FXML
     private void clickCancelButton(ActionEvent event) throws IOException {
@@ -21,4 +59,66 @@ public class PartController {
         appStage.show();
     }
 
+    @FXML
+    private void inHouseSelected() {
+        miscName.setText("Machine ID");
+        if (outSourcedButton.isSelected()) {
+            outSourcedButton.setSelected(false);
+        }
+    }
+
+    @FXML
+    private void outsourcedSelected() {
+        miscName.setText("Company Name");
+        if (inHouseButton.isSelected()) {
+            inHouseButton.setSelected(false);
+
+        }
+    }
+
+    @FXML
+    private void clickAddSavePart() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../fxml/MainMenu.fxml"));
+        MainMenuController mainMenuController = loader.getController();
+        if (inHouseButton.isSelected()) {
+            InHouse inHouse = new InHouse(mainMenuController.partId,
+                    partName.getText(),
+                    Integer.parseInt(partCost.getText()),
+                    Integer.parseInt(partInv.getText()),
+                    Integer.parseInt(partMin.getText()),
+                    Integer.parseInt(partMax.getText()),
+                    Integer.parseInt(partCompany.getText()));
+
+            Stage stage;
+            Parent root;
+            stage = (Stage) savePartButton.getScene().getWindow();
+            root = loader.load();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+            mainMenuController.addPart(inHouse);
+            mainMenuController.partId++;
+        }
+
+        if (outSourcedButton.isSelected()) {
+            OutSourced outSourced = new OutSourced(mainMenuController.partId,
+                    partName.getText(),
+                    Integer.parseInt(partCost.getText()),
+                    Integer.parseInt(partInv.getText()),
+                    Integer.parseInt(partMin.getText()),
+                    Integer.parseInt(partMax.getText()),
+                    partCompany.getText());
+
+            Stage stage;
+            Parent root;
+            stage = (Stage) savePartButton.getScene().getWindow();
+            root = loader.load();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+            mainMenuController.addPart(outSourced);
+            mainMenuController.partId++;
+        }
+
+    }
 }
